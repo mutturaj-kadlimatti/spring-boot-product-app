117a69f27300b12b7128f90b45067432e0c8be2f
Mutturaj Kadlimatti <mutturaj.kadlimatti@gmail.com>
2026-01-11 20:07:43 +0530

    Revert "hello-134"
    This reverts commit b8315ecabb5247b326e6ebb1607c8cc491041556.

--- src/main/java/com/javatechie/controller/ProductController.java
+++ src/main/java/com/javatechie/controller/ProductController.java
@@ -25,7 +25,7 @@ public class ProductController {
     public static final String SUCCESS = "Success";
     private ProductService productService;
 
-/// 134
+///
 
     @PostMapping
     public ResponseEntity<APIResponse> createNewProduct(@RequestBody @Valid ProductRequestDTO productRequestDTO) {
